b3ca268d834d2ace1498680f3880c73a9bd13b4d
Jostein Tysse <josteiht@stud.ntnu.no>
2021-01-05 20:55:40 +0100

    Add Cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(
     ":method :url :status :res[content-length] - :response-time ms :content"
   )
 );
+app.use(cors());
 
 let persons = [
   {
