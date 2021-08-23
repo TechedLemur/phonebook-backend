2be5701565070ade6eebbbca6a0282560f19d145
Jostein Tysse <josteiht@stud.ntnu.no>
2021-08-23 23:31:12 +0200

    Feat delete via mongoose

--- index.js
+++ index.js
@@ -70,11 +70,12 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -91,9 +92,9 @@ app.post("/api/persons", (request, response) => {
       error: "Number is missing",
     });
 
-    const _person = new Person({
-      name: person.name,
-      number: person.number,
+  const _person = new Person({
+    name: person.name,
+    number: person.number,
   })
 
   _person.save().then(savedPerson => {
