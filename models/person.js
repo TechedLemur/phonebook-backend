613533e6fa91963f3b9e480d3656310847884c93
Jostein <josteiht@stud.ntnu.no>
2021-09-01 21:21:11 +0200

    Add unique mongoose validator

--- models/person.js
+++ models/person.js
@@ -1,22 +1,30 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
+const uniqueValidator = require("mongoose-unique-validator");
 
-console.log('connecting to database')
+console.log("connecting to database");
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
-
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-})
+  name: { type: String, required: true, unique: true, minLength: 3 },
+  number: { type: String, required: true, unique: true, minLength: 8 },
+});
+
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
